dacc919d3ed2646d462f7b201c8be90b6052d202
Alan Stebbens <aks@stebbens.org>
2013-08-01 14:11:10 -0700

    Added TODO; reorganized; created help function; added fn_exists.

--- sh-utils.sh
+++ sh-utils.sh
@@ -5,108 +5,129 @@
 # Copyright 2006-2013 Alan K. Stebbens <aks@stebbens.org>
 #
 #
-# chat MSG ..
-#
-#   If $norun or $verbose is set, print all args on STDERR.
-#
-# talk MSG ..
-#
-#   Print all arguments on STDERR.
-#
-# nvtalk MSG 
-#
-#   Print all arguments on STDERR only if $verbose is not set.
-#
-# error [CODE] "MSG" 
-#
-#   Print MSG on STDERR, then exit with code CODE (or 2)
-#
-# run COMMAND ARGS ..
-# 
-# If $verbose is set, show the command and args before running it.
-# if $norun is not set, run the command with args and examine the resulting status.
-#
-#
+
+[[ -z "$SH_UTILS" ]] || return
+
+export SH_UTILS=${BASH_SOURCE[0]}
+
+sh_utils_help() {
+    cat 1>&2 <<EOF
+Shell utility functions:
+
+talk MSG ..         Print all arguments on STDERR.
+chat MSG ..         If $norun or $verbose is set, print all args on STDERR.
+nvtalk MSG          Print all arguments on STDERR only if $verbose is not set.
+error [CODE] "MSG"  Print MSG on STDERR, then exit with code CODE (or 2)
+
+talkf FMT ARGS ..           printf FMT ARGS on STDERR
+chatf FMT ARGS ..           printf FMT ARGS on STDERR if $norun or $verbose set
+nvtalkf FMT ARGS ..         printf FMT ARGS on STDERR unless $verbose set
+errorf [CODE] FMT ARGS ..   printf FMT ARGS on STDERR, then exit $CODE [2]
+
+run COMMAND ARGS .. Show COMMAND ARGS if $norun or $verbase; run command unless $norun.
+
+rm_file_later FILE  Cause FILE to be removed upon program exit.
+
+add_trap "CMD" SIGNAL ..   Add CMD to the trap list for SIGNAL
+
+fn_exists FUNCTION         return 0 (true) if FUNCTION exists; 1 (false) otherwise
+
+EOF
+}
+help_sh_utils() { sh_utils_help ; }
+
+# chat MSG        - show MSG on STDERR if $norun or $verbose
+# talk MSG        - show MSG on STDERR
+# warn MSG        - show MSG on STDERR
+# nvtalk MSG      - show MSG on STDERR unless $verbose is set
+
+talk()        { echo 1>&2 "$@" ; }
+warn()        { talk "$@" ; }
+chat()	{ [[ -n "$norun$verbose" ]] && talk "$@" ; }
+nvtalk()      { [[ -z "$verbose" ]] && talk "$@" ; }
+
+# error [CODE] MSG - show MSG on STDERR then exit with error CODE [default 2]
+
+error()       { 
+  local code=2
+  case "$1" in [0-9]*) code=$1 ; shift ;; esac
+  talk "$@"
+  exit $code
+}
+
+# chatf FMT ARGS..    printf FMT ARGS on STDERR if $norun or $verbose are set
+# talkf FMT ARGS...   printf FMT ARGS on STDERR
+# warnf FMT ARGS...   alias for talkf
+# nvtalkf FMT ARGS..  printf FMT ARGS on STDERR unless $verbose set
+
+talkf()       { printf 1>&2 "$@" ; }
+warnf()       { talkf "$@" ; }
+chatf()       { [[ -n "$norun$verbose" ]] && talkf "$@" ; }
+nvtalkf()     { [[ -z "$verbose" ]] && talkf "$@" ; }
+
+# errorf [CODE] FMT ARGS .. print FMT ARGS on STDERR, then exit with CODE[2]
+
+errorf()      { 
+  local code=2
+  case "$1" in [0-9]*) code=$1 ; shift ;; esac
+  talkf "$@"
+  exit $code
+}
+
 # rm_file_later FILE
 #
-#   Add FILE to a list of files that will be automatically removed
-#   upon program exit.
-#
-# add_trap "CMD" SIGNAL ..
-#
-#   Add CMD to the trap list for SIGNAL, while ensuring that it is
-#   not repeated.
+# schedule file for later removal on program exit
 
-if [[ "$SH_UTILS" != 'true' ]]; then
-  SH_UTILS=true
+rm_file_later() {
+  local rmfiles="/tmp/rmfiles-$$.sh"
+  touch $rmfiles
+  ( fgrep -v $rmfiles $rmfiles ; echo "/bin/rm -f '$1'" ; echo "/bin/rm -f $rmfiles" ) > $rmfiles.new
+  mv -f $rmfiles.new $rmfiles
+  add_trap "/bin/sh $rmfiles"  EXIT HUP INT TERM
+  add_trap "exit"              EXIT HUP INT TERM
+}
 
-  chat()	{ if [[ -n "$norun$verbose" ]]; then echo 1>&2 "$@" ; fi ; }
-  talk()        { echo 1>&2 "$@" ; }
-  nvtalk()      { if [[ -z "$verbose" ]]; then echo 1>&2 "$@" ; fi ; }
+# add_trap "Command" SIGNAL ..
 
-  error()       { 
-    local code=2
-    case "$1" in [0-9]*) code=$1 ; shift ;; esac
-    echo 1>&2 "$@"
-    exit $code
-  }
-
-  chatf()       { if [[ -n "$norun$verbose" ]]; then printf 1>&2 "$@" ; fi ; }
-  talkf()       { printf 1>&2 "$@" ; }
-  nvtalkf()     { if [[ -z "$verbose" ]]; then printf 1>&2 "$@" ; fi ; }
-
-  errorf()      { 
-    local code=2
-    case "$1" in [0-9]*) code=$1 ; shift ;; esac
-    printf 1>&2 "$@"
-    exit $code
-  }
-
-  # rm_file_later FILE
-  #
-  # schedule file for later removal on program exit
-
-  rm_file_later() {
-    local rmfiles="/tmp/rmfiles-$$.sh"
-    touch $rmfiles
-    ( fgrep -v $rmfiles $rmfiles ; echo "/bin/rm -f '$1'" ; echo "/bin/rm -f $rmfiles" ) > $rmfiles.new
-    mv -f $rmfiles.new $rmfiles
-    add_trap "/bin/sh $rmfiles"  EXIT HUP INT TERM
-    add_trap "exit"              EXIT HUP INT TERM
-  }
-
-  # add_trap "Command" SIGNAL ..
-
-  add_trap() {
-    local cmd="$1" ; shift
-    for sig in "$@" ; do
-      traps="`trap -p $sig | tr ';' '\n' | grep -v \"$cmd\" | tr '\n' ' ; '`"
-      if [[ -n "$traps" ]]; then
-        trap "$traps ; $cmd" $sig
-      else
-        trap "$cmd" $sig
-      fi
-    done
-  }
-
-  # run COMMAND ARGS ...
-
-  run() {
-    if [[ $norun ]]; then
-      talk "(norun) $@"
+add_trap() {
+  local cmd="$1" ; shift
+  for sig in "$@" ; do
+    traps="`trap -p $sig | tr ';' '\n' | grep -v \"$cmd\" | tr '\n' ' ; '`"
+    if [[ -n "$traps" ]]; then
+      trap "$traps ; $cmd" $sig
     else
-      if [[ $verbose ]]; then
-        talk ">> $@"
-      fi
-      if ! eval "$@" ; then
-        local code=$?
-        return $code
-      fi
+      trap "$cmd" $sig
     fi
-    return 0
-  }
+  done
+}
 
-fi
+# run COMMAND ARGS ...
+
+run() {
+  if [[ $norun ]]; then
+    talk "(norun) $@"
+  else
+    safe_run "$@"
+  fi
+  return 0
+}
+
+# safe_run COMMAND ARGS
+# Safe run -- run command even in "$norun" mode
+
+safe_run() {
+  if [[ -n "$verbose$norun" ]]; then
+    talk ">> $@"
+  fi
+  if ! eval "$@" ; then
+    code=$?
+    return $code
+  fi
+  return 0
+}
+
+# fn_exists FUNCTION
+fn_exists() { declare -f "$1" >/dev/null ; }
 
 # end of sh-utils.sh
 # vim: sw=2 ai

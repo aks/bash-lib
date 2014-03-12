1ee91273256242c345924146ebd9188f42c48dd3
Alan Stebbens <aks@stebbens.org>
2014-03-12 09:41:11 -0700

    aks: new files: bash-lib.sh reset-util-vars.sh

---
+++ bash-lib.sh
@@ -0,0 +1,13 @@
+# bash-lib.sh
+# Copyright 2009-2014 Alan K. Stebbens <aks@stebbens.org>
+#
+# source all the bash library files
+
+source sh-utils.sh
+source date-utils.sh
+source hash-utils.sh
+source list-utils.sh
+source real-utils.sh
+source text-utils.sh
+
+# sync-files.sh   # not generally useful yet
---
+++ reset-util-vars.sh
@@ -0,0 +1,18 @@
+# reset-util-vars.sh
+#
+# each module checks a variable to avoid recursive sourcing.  These variables
+# are not exported, so subshells will always source correctly.
+#
+# This function definition must be sourcd into bash, and then the function
+# invoked in order to clear these variables within the current bash context.
+
+reset_util_vars() {
+  unset CALENDAR_UTILS_SH
+  unset DATE_UTILS_SH
+  unset HASH_UTILS_SH
+  unset LIST_UTILS_SH
+  unset REAL_UTILS_SH
+  unset SH_UTILS_SH
+  unset TEST_UTILS_SH
+  unset TEXT_UTILS_SH
+}

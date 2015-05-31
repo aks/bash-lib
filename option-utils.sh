ad3b653cc12d45f0f241765358197e47d2b31a6d
Alan Stebbens <aks@stebbens.org>
2015-05-31 00:27:10 -0700

    new module for managing option lists

---
+++ option-utils.sh
@@ -0,0 +1,79 @@
+# option-utils.sh -- 
+# Copyright 2015 Alan K. Stebbens <aks@stebbens.org>
+
+# utility for collecting lists of options and arguments
+
+OPTION_UTILS_VERSION="option-utils.sh v1.0"
+[[ "$OPTION_UTILS_SH" = "$OPTION_UTILS_VERSION" ]] && return
+OPTION_UTILS_SH="$OPTION_UTILS_VERSIONS"
+
+option_help() {
+  cat 1>&2 <<'EOF'
+The option-util.sh library is a small set of functions to manage
+building options and arguments, which is often needed in the
+development of command-line utilities.  
+
+These functions use two global variables: `option_pairs` and
+`options`.  The `option_pairs` variable is used to accumulate pairs
+of options and arguments, eg: "-F FILE", while `options` is used to
+accumulate single character options that can be clustered behind a
+single dash "-".
+
+All of the accumulated options and arguments can be output with
+`all_opts`.
+
+init_opts                # empty "option_pairs" and "options"
+
+reset_opts               # same as init_opts
+
+add_optarg OPTION ARG .. # add OPTION and ARG to the option_pairs list
+
+add_option OPTION ..    # add OPTION to the single options list
+add_opt    OPTION ..    # eg: add_arg -c -d ..  or add_arg c d ..
+
+all_opts                 # outputs both option_pairs and options
+EOF
+}
+help_option() { option_help ; }
+
+source help-util.sh
+
+init_options() {
+  declare -g option_pairs=
+  declare -g options='-'         # a list of clustered single options
+}
+
+reset_options() { init_options ; }
+
+add_optarg() {
+  help_args_func option_help $# 2 || return
+  while (( $# > 0 )); do
+    case "$1" in
+      -*) option_pairs+=" $1 $2" ;;
+      *)  option_pairs+=" -$1 $2" ;;
+    esac
+    shift 2
+  done
+}
+
+add_option() {
+  help_args_func option_help $# 1 || return
+  while (( $# > 0 )); do
+    case "$1" in
+      -*) options+="${1#-}" ;;      # append option without leading '-'
+      *)  options+="$1" ;;
+    esac
+    shift
+  done
+}
+add_opt() { add_option "$@" ; }
+
+all_args() {
+  if (( ${#options} == 1 )); then     # don't output empty '-'
+    echo "$option_pairs"
+  else
+    echo "$option_pairs $options"          # output both the option_pairs and options
+  fi
+}
+
+# end of option-utils.sh

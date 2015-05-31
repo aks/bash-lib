3ea1df643640a6b3a46915585c3c53e9a1d3a9d9
Alan Stebbens <aks@stebbens.org>
2015-05-30 23:41:32 -0700

    added newly refactored modules

--- bash-lib.sh
+++ bash-lib.sh
@@ -1,13 +1,18 @@
 # bash-lib.sh
-# Copyright 2009-2014 Alan K. Stebbens <aks@stebbens.org>
+# Copyright 2009-2015 Alan K. Stebbens <aks@stebbens.org>
 #
 # source all the bash library files
 
+source help-util.sh
+source arg-utils.sh
 source sh-utils.sh
 source date-utils.sh
 source hash-utils.sh
 source list-utils.sh
 source real-utils.sh
+source run-utils.sh
+source option-utils.sh
+source talk-utils.sh
 source text-utils.sh
 
 # sync-files.sh   # not generally useful yet

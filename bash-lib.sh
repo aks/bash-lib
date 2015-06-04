80bcf702b1bf75d51f914a9d77ae4da02ba548b8
Alan Stebbens <aks@stebbens.org>
2015-06-04 08:19:02 +0000

    added missing scripts

--- bash-lib.sh
+++ bash-lib.sh
@@ -5,14 +5,19 @@
 
 source help-util.sh
 source arg-utils.sh
-source sh-utils.sh
+source bash-check.sh
+source calendar-utils.sh
 source date-utils.sh
 source hash-utils.sh
 source list-utils.sh
-source real-utils.sh
-source run-utils.sh
 source option-utils.sh
+source real-utils.sh
+source reset-util-vars.sh
+source run-utils.sh
+source sh-utils.sh
 source talk-utils.sh
+source test-utils.sh
 source text-utils.sh
+source time-utils.sh
 
 # sync-files.sh   # not generally useful yet

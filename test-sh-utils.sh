7a1f7c6a47844d550f00ed06d825728bf12956c3
Alan Stebbens <aks@stebbens.org>
2015-09-05 20:08:46 -0700

    added some trap tests

--- test-sh-utils.sh
+++ test-sh-utils.sh
@@ -6,19 +6,37 @@
 # Test module for sh-utils.sh
 #
 export PATH=.:$PATH
+
 source sh-utils.sh
 source test-utils.sh
 
-
-wordlist=( the time has come to talk of ceiling wax and cabbages and kings )
-
-test_01_first_sh_test() {
+test_01_fn_exists() {
   start_test
+  check_false "fn_exists TEST_test_func"
+  # create the test func
+  TEST_test_func() { my_test_ok=1 ; }
+  check_true 'fn_exists TEST_test_func'
+  unset -f TEST_test_func
+  check_false 'fn_exists TEST_test_func'
   end_test
 }
 
-test_10_second_sh_test() {
+test_10_add_trap() {
   start_test
+  local x=$( my_test_ok=2
+             add_trap 'my_test_ok=3' USR1
+             kill -USR1 $BASHPID
+             echo "$my_test_ok"
+           )
+  check_equal $x 3
+  local y=$( my_test_ok=4 my_new_test=1
+             add_trap 'my_test_ok=3'  USR1
+             add_trap 'my_new_test=2' USR1
+             kill -USR1 $BASHPID
+             sleep .5
+             echo "$my_test_ok $my_new_test"
+           )
+  check_equal "$y" "3 2"
   end_test
 }
 

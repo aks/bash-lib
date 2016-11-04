3ee34dddb034c9d26d29355fdd1dbd22cec29db4
Alan Stebbens <alan.stebbens@procore.com>
2016-11-04 14:08:56 -0700

    fixed add_trap to work reliably

--- run-utils.sh
+++ run-utils.sh
@@ -49,17 +49,23 @@ rm_file_later() {
 
 # add_trap "Command" SIGNAL ..
 
+# add_trap CMD [SIGNAL ...]
 add_trap() {
-  help_args_func run_utils_help $# 2 || return 1
   local cmd="$1" ; shift
+  local sig cmds
   for sig in "$@" ; do
-    traps="`trap -p $sig | tr ';' '\n' | grep -v \"$cmd\" | tr '\n' ' ; '`"
-    if [[ -n "$traps" ]]; then
-      trap "$traps ; $cmd" $sig
-    else
-      trap "$cmd" $sig
-    fi
+    cmds=`trap_cmd $sig`
+    cmds="$cmds${cmds:+; }$cmd"
+    trap "$cmds" $sig
   done
+}
+
+# trap_cmd SIGNAL
+trap_cmd() {
+  local cmds=`trap -p $1`
+  cmds="${cmds#*\'}"
+  cmds="${cmds%\'*}"
+  echo "$cmds"
 }
 
 # run COMMAND ARGS ...
